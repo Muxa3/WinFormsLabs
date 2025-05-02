8baeb90ae2510124732b8c3a004c03aa9593f3d5
Muxa3 <MislamD@mail.ru>
2025-05-03 02:23:58 +0300

    Theme4 update

--- Theme3/Lab1/AdditionalExercises/Task2/obj/Debug/netcoreapp3.1/Task2.AssemblyInfo.cs
+++ Theme3/Lab1/AdditionalExercises/Task2/obj/Debug/netcoreapp3.1/Task2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b452a448f11e88d9ea71a2ba8d37a3d57b150019")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
