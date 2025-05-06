d3dddf5ee084f4bfedc3b484410356821fa40262
Muxa3 <MislamD@mail.ru>
2025-05-06 22:53:34 +0300

    Theme4 update (+ labs)

---
+++ Theme4/Lab3/WebApplication1/Controllers/Lab1Controller.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class Lab1Controller : Controller
+    {
+        // GET: Lab1
+        public ActionResult FirstViewMethod()
+        {
+            List<string> vegetables = GetVegetableList();
+            return View(vegetables);
+        }
+        public ActionResult SecondViewMethod()
+        {
+            return View(GetVegetableList().OrderBy(x => x).ToList());
+        }
+
+        public ActionResult ThirdViewMethod()
+        {
+            return View(GetVegetableList().OrderBy(x => x).ToList());
+        }
+
+        public List<string> GetVegetableList()
+        {
+            List<string> vegetables = new List<string>();
+            vegetables.Add("Томат");
+            vegetables.Add("Огурец");
+            vegetables.Add("Картофель");
+            vegetables.Add("Кабачок");
+            vegetables.Add("Баклажан");
+            vegetables.Add("Капуста");
+            vegetables.Add("Брокколи");
+
+            return vegetables;
+        }
+    }
+}
---
+++ Theme4/Lab3/WebApplication1/Models/ViewModels/PersonVM.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+
+namespace WebApplication1.Models.ViewModels
+{
+    public class PersonVM
+    {
+        public System.Guid Id { get; set; }
+
+        [Required]
+        [DisplayName("Фамилия")]
+        [StringLength(100, MinimumLength = 2)]
+        public string LastName { get; set; }
+
+        [Required]
+        [DisplayName("Имя")]
+        public string FirstName { get; set; }
+
+        [DisplayName("Отчество")]
+        public string Patronymic { get; set; }
+
+        [Required]
+        [Range(18, 100)]
+        [DisplayName("Полных лет")]
+        public int Age { get; set; }
+
+        [DisplayName("Пол")]
+        public string Gender { get; set; }
+
+        [Required]
+        [DisplayName("Трудоустроен")]
+        public bool HasJob { get; set; }
+    }
+}
